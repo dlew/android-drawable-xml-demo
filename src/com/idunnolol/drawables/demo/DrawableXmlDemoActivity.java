1dfc2269a11efd748fe791317b326e9fad8a05f9
Daniel Lew <danlew42@gmail.com>
2011-09-28 16:43:31 -0500

    Initial project

---
+++ src/com/idunnolol/drawables/demo/DrawableXmlDemoActivity.java
@@ -0,0 +1,12 @@
+package com.idunnolol.drawables.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrawableXmlDemoActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
